52052704d7ba2ef3c1ae45bb7a10efec7aa4c97e
Carl Öst Wilkens <carl@ostwilkens.se>
2022-09-24 19:53:16 +0200

    WIP

--- program_g.py
+++ program_g.py
@@ -15,26 +15,30 @@ import sys
 # "type[alias_a|alias_b]"
 
 
-def _program_0(a: "text") -> "text[reversed|mirrored]":
+# def _program_0(a: "text") -> "text[reversed|mirrored]":
+def _program_0(a: "text") -> "reversed text":
     return a[::-1]
 
 
-def _program_1(a: "number", b: "number") -> "number[sum|total]":
-    return a + b
+# def _program_1(a: "number", b: "number") -> "total number":
+#     return a + b
 
 
-def _program_2(a: "text[math_problem]") -> "number[solution]":
-    # TODO: replace eval
-    return eval(a)
+# def _program_2(a: "text[math_problem]") -> "number[solution]":
+#     # TODO: replace eval
+#     return eval(a)
 
 
-def _program_3(a: "text") -> None:
+def _program_3(a: "text") -> "display none":
     print(a)
 
 
-def _program_4(a: "text[user]") -> "image[user]":
-    # TODO: implement
-    return None
+# def _program_4(a: "text[user]") -> "image[user]":
+#     # TODO: implement
+#     return None
+
+def _program_5() -> "user text":
+    return input("Input: ")
 
 # build program db
 def annotations_to_signature(annotations: dict) -> str:
@@ -82,13 +86,13 @@ def do_x_to_y(x, y):
     return find_programs_for_goal(human_goal)[0](y)
 
 
-do_x_to_y("reverse text", "hehu")
-do_x_to_y("solve math_problem", "1+1")
+# do_x_to_y("reverse text", "hehu")
+# do_x_to_y("solve math_problem", "1+1")
 
 # machine goals
 
 # "[action ]([input_alias, input alias ]input_type)[ -> ([return_alias] return_type)]""
-# action is taken on
+# action is a consuming function without return
 
 # a simple program that reverses input text and returns it
 machine_goal_a = "(text) -> (reversed text)"
@@ -96,8 +100,33 @@ machine_goal_a = "(text) -> (reversed text)"
 # a program that displays input text
 machine_goal_b = "display (text)"
 
-# a program that takes user text input and displays it reversed
-machine_goal_c = "display (reversed user text)"
+# # a program that takes user text input and displays a generated image
+# machine_goal_d = "display (user from_text image)"
 
-# a program that takes user text input and displays a generated image
-machine_goal_d = "display (user from_text image)"
+# a program that takes user text input and displays it reversed
+# machine_goal_c = "display (reversed user text)"
+machine_goal_c = "(display reversed user text)"
+
+# produce (reversed user text)
+x = _program_5()  # () -> (user text)
+xt = "user text"
+x = _program_0(x) # (text) -> (reversed text)
+xt = "reversed user text"
+x = _program_3(x) # (text) -> (display none)
+xt = "display reversed user text"
+
+
+# a program which rotates an image by n (user input) degrees
+machine_goal_e = "(rotated) image"
+
+
+# a program which combines two user-input strings
+machine_goal_f = "user text + user text"
+
+# a program which hashes a string with a (user input) seed string
+machine_goal_g = "hash user text with user text"
+
+# the 'goal language' doesn't necessarily need to be english
+machine_goal_h = "hash(#user text, #user text)"
+
+
